5acc0cd4c8f738b8c4a9418963535dd4f8d4951b
Sarpar <dong.ashton@gmail.com>
2022-05-13 21:40:15 -0400

    returns value of init_value!

--- array.py
+++ array.py
@@ -13,8 +13,17 @@ class matrix:
         self.array1 = [0] * self.columns       # This creates the matrix
         for i in range (self.columns):
             self.array1[i] = [0] * self.rows
-        print(self.array1)
+        
 
-mat1 = matrix(3, 3)
-mat1.create()
+def init_value():
+    rows = int(input("How many rows? "))
+    columns = int(input("How many columns? "))
+    matrices1 = matrix(rows, columns)
+    matrices1.create()
+    return matrices1.array1
 
+init_value()
+
+def row_switch():
+    row1 = input("Row 1? ")
+    row2 = input("Row 2? ")
