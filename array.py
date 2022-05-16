fc88f92305be28d6cf0d058e2089f997135911a7
DESKTOP-BPDPDML\Ashton <dong.ashton@gmail.com>
2022-05-16 07:58:00 -0400

    Initial Populate Function

--- array.py
+++ array.py
@@ -9,21 +9,26 @@ class matrix:
 
 
     def create(self):
-
         self.array1 = [0] * self.columns       # This creates the matrix
         for i in range (self.columns):
             self.array1[i] = [0] * self.rows
         
 
-def init_value():
+def init_value():       # Returns matrix with number of rows and columns filled with 0's as filled_matrix
     rows = int(input("How many rows? "))
     columns = int(input("How many columns? "))
     matrices1 = matrix(rows, columns)
     matrices1.create()
-    return matrices1.array1
+    global filled_matrix        # makes filled matrix a global variable
+    filled_matrix = matrices1.array1        # takes array1 from filled_matrix, which was just filled with 0's
 
 init_value()
 
-def row_switch():
-    row1 = input("Row 1? ")
-    row2 = input("Row 2? ")
+def populate():
+    for i in filled_matrix:
+        row_number = row_number + 1
+        n_o_t = (len(i) * -1)    # n_o_t stands for number of terms. len(i) is multiplied by -1 because the negative of len(i) will give the 0th index term
+        if n_o_t <= -1:
+            nth_term = (len(i) - (1 - len(i))) # nth term is the term number for number in a row, starting at index 1
+            i[n_o_t] = input('Number for Term #' + str(nth_term) + 'in Row #' + str(row) + ' ?') 
+            n_o_t = n_o_t + 1
