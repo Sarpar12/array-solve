775d928dc023954c5d4b3f3b93b423e632d36a37
Sarpar <dong.ashton@gmail.com>
2022-07-29 14:22:36 -0400

    fixed Issue #1

--- Numpy-Array.py
+++ Numpy-Array.py
@@ -6,18 +6,23 @@ class matrix:
     def __init__(self):
 
         # Create an array with 0 rows and 0 columns
-        array1 = np.empty(0,0) 
+        self.array1 = np.empty
         rows = int(input("How many rows is the matrix? ")) 
-        colums = int(input("How many colums is the matrix? "))
+        columns = int(input("How many columns is the matrix? "))
 
         # Takes Input of "1 2 3"(note the spaces,  this is funcinput), uses map(func, funcinput) to apply int() to funcinput
         # Funcinput is split with " " as the separator and tuns funcinput to ['1', '2', '3']
         list_of_values = list(map(int, input('Input Matrix values, Ex: "1 2 3" ').split()))
-        array1 = np.array(list_of_values).reshape(rows,columns)
-        return array1 
+        self.array1 = np.array(list_of_values).reshape(rows,columns)
 
+    # Throws Error, might need to be moved out of class
     def display_matrix(self):
         print(array1) # Naturally fomatted with rows and columns due to numpy
 
     # Needs more work for all functions below comment
-    def row_col_swap(self, row1, row2)
+    def row_swap(self):
+        I_row = int(input("Original Row? "))
+        A_row = int(input("Row to swap? "))
+        
+
+
