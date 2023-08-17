ccc4deefb0fc26fa5e183503f1ec64d1dc2af5b7
Ashton Dong <66439777+Sarpar12@users.noreply.github.com>
2023-08-16 20:36:52 -0500

    added a print function

--- Numpy-Array.py
+++ Numpy-Array.py
@@ -3,7 +3,7 @@ import numpy as np
 class matrix: 
     # This functions creates matrix
     def __init__(self,rows,columns):
-        self.array1 = np.empty # Create an array with 0 rows and 0 columns
+        self.wArray = np.empty # Create an array with 0 rows and 0 columns
         self.addition_buffer_row_column = np.empty # Used to store multiplicated rows or columns, and then added
         self.subtraction_buffer_row_column = np.empty # Used to store multiplicated rows or columns, and then subtracted
         self.rows = rows 
@@ -14,10 +14,10 @@ class matrix:
     def reshape_and_fill(self): 
         # Takes Input of "1 2 3"(note the spaces,  this is funcinput), uses map(func, funcinput) to apply int() to funcinput
         # Funcinput is split with " " as the separator and tuns funcinput to ['1', '2', '3']
-        list_of_values = list(map(int, input('Input Matrix values, Ex: "1 2 3"\n ').split()))
+        list_of_values = list(map(int, input('Input Matrix values, Ex: "1 2 3"\n').split()))
         if len(list_of_values) == (self.rows * self.columns):
-            self.array1 = np.array(list_of_values).reshape(self.rows,self.columns)
-            return self.array1
+            self.wArray = np.array(list_of_values).reshape(self.rows,self.columns)
+            return self.wArray
         else:
             print("Too much or too little terms!")
             return self.reshape_and_fill() # Loops function if # of terms doesnt match what should be there
@@ -25,80 +25,93 @@ class matrix:
     def row_swap(self):
         I_row = (int(input("Original Row? ")) - 1)
         A_row = (int(input("Row to swap? ")) - 1)
-        self.array1[[I_row, A_row]] = self.array1[[A_row, I_row]]
-        return self.array1
+        self.wArray[[I_row, A_row]] = self.wArray[[A_row, I_row]]
+        return self.wArray
     
     def col_swap(self): 
         I_column = (int(input("Original column? ")) - 1)
         A_column = (int(input("column to swap? ")) - 1)
-        self.array1[:, [I_column, A_column]] = self.array1[:, [A_column, I_column]]
-        return self.array1
+        self.wArray[:, [I_column, A_column]] = self.wArray[:, [A_column, I_column]]
+        return self.wArray
 
     def multiply_row(self):
         row = (int(input("Which Row? ")) - 1)
         multiple = (int(input("What Number? ")))
-        self.array1[row, :] = np.multiply(self.array1[row, :],multiple) # Yes, you couse use *, but I like this for readablity
-        return self.array1
+        self.wArray[row, :] = np.multiply(self.wArray[row, :],multiple) # Yes, you couse use *, but I like this for readablity
+        return self.wArray
 
     def multiply_column(self):
         column = (int(input("Which column? ")) - 1)
         multiple = (int(input("What Number? ")))
-        self.array1[:, column] = np.multiply(self.array1[:, column],multiple) # ":" means select all rows or columns depending on where it is placed
-        return self.array1
+        self.wArray[:, column] = np.multiply(self.wArray[:, column],multiple) # ":" means select all rows or columns depending on where it is placed
+        return self.wArray
     
     def row_addition(self): # Might need subarrays, numpy doesnt seem to provide a way to add values within the same matrix
         I_row = (int(input("Row to be added to? ")) - 1)
         A_row = (int(input("Row added to other row? ")) - 1)
         A_row_multiple = int(input("What is 2nd row multplied by? "))
-        self.addition_buffer_row_column = np.multiply(self.array1[A_row, :], A_row_multiple)
-        self.array1[I_row, :] = np.array([term1 + term2 for term1, term2 in zip(self.array1[I_row, :], self.addition_buffer_row_column)])
+        self.addition_buffer_row_column = np.multiply(self.wArray[A_row, :], A_row_multiple)
+        self.wArray[I_row, :] = np.array([term1 + term2 for term1, term2 in zip(self.wArray[I_row, :], self.addition_buffer_row_column)])
         self.addition_buffer_row_column = np.empty
-        return self.array1
+        return self.wArray
     
     def col_addition(self):
         I_col = (int(input("column to be added to? ")) - 1)
         A_col = (int(input("column added to other column? ")) - 1)
         A_col_multiple = int(input("What is 2nd column multplied by? "))
-        self.addition_buffer_row_column = np.multiply(self.array1[:, A_col], A_col_multiple)
-        self.array1[:, I_col] = np.array([term1 + term2 for term1, term2 in zip(self.array1[:, I_col], self.addition_buffer_row_column)])
+        self.addition_buffer_row_column = np.multiply(self.wArray[:, A_col], A_col_multiple)
+        self.wArray[:, I_col] = np.array([term1 + term2 for term1, term2 in zip(self.wArray[:, I_col], self.addition_buffer_row_column)])
         self.addition_buffer_row_column = np.empty
-        return self.array1
+        return self.wArray
 
     def row_subtraction(self):
         I_row = (int(input("Row to be subtracted from? ")) - 1)
         A_row = (int(input("Row used to subtract? ")) - 1)
         A_row_multiple = int(input("What is 2nd row multplied by? "))
-        self.subtraction_buffer_row_column = np.multiply(self.array1[A_row, :], A_row_multiple)
-        self.array1[I_row, :] = np.array([term1 - term2 for term1, term2 in zip(self.array1[I_row, :], self.subtraction_buffer_row_column)])
+        self.subtraction_buffer_row_column = np.multiply(self.wArray[A_row, :], A_row_multiple)
+        self.wArray[I_row, :] = np.array([term1 - term2 for term1, term2 in zip(self.wArray[I_row, :], self.subtraction_buffer_row_column)])
         self.subtraction_buffer_row_column = np.empty
-        return self.array1
+        return self.wArray
     
     def col_subtraction(self): 
         I_col = (int(input("column to be subtracted from? ")) - 1)
         A_col = (int(input("column used to subtract? ")) - 1)
         A_col_multiple = int(input("What is 2nd col multplied by? "))
-        self.subtraction_buffer_row_column = np.multiply(self.array1[:, A_col], A_col_multiple)
-        self.array1[:, I_col] = np.array([term1 - term2 for term1, term2 in zip(self.array1[:, I_col], self.subtraction_buffer_row_column)])
+        self.subtraction_buffer_row_column = np.multiply(self.wArray[:, A_col], A_col_multiple)
+        self.wArray[:, I_col] = np.array([term1 - term2 for term1, term2 in zip(self.wArray[:, I_col], self.subtraction_buffer_row_column)])
         self.subtraction_buffer_row_column = np.empty
-        return self.array1
+        return self.wArray
     
     def deter_finder(self):
-        return int(np.linalg.det(self.array1))
+        return int(np.linalg.det(self.wArray))
     
-    def inv_finder(): # Will give floats, not sure how to solve
+    # def inv_finder(): # Will give floats, not sure how to solve
 
-    
+def makeMatrix():
+    inputNums = list(map(int, input('Input RxC, Ex: "R C"\n').split()))
+    while len(inputNums) < 2: # Input validation 
+        inputNums = list(map(int, input('Input RxC, Ex: "R C"\n').split()))
+    row, column = inputNums[0], inputNums[1] 
+    tempMatrix = matrix(row, column)
+    tempMatrix.reshape_and_fill()
+    ### print(tempMatrix.wArray)
+    return tempMatrix
 
-        
-    
+def printMatrix():
+    printString = "["
+    nums = globalMatrix.wArray.shape
+    rows = nums[0]
+    cols = nums[1]
+    for i in range(rows):
+        printString = printString + "\n"
+        for j in range(cols):
+            printString = printString + str(globalMatrix.wArray[i][j]) + " "
+    printString = printString + "\n]"
+    print(printString)
 
 def start():
-    rows = int(input("How many rows? "))
-    columns = int(input("How many Columns? "))
-    Matrix_Use = matrix(rows, columns)
-    Matrix_Use.reshape_and_fill()
-    print(Matrix_Use.array1.shape)
-    input()
-    print(Matrix_Use.deter_finder())
+    global globalMatrix
+    globalMatrix = makeMatrix()
+    printMatrix()
 
 start()
