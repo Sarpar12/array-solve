5408fa7d905db95d2a0094c794cd9ad3e82e408d
Ashton Dong <dong.ashton@gmail.com>
2024-06-12 18:44:03 -0500

    Minor Addition: main() in progress
    - do_operation() in progress
    - fixed variables not in snakecase

--- main.py
+++ main.py
@@ -50,18 +50,69 @@ def create_storage(matrix: Matrix | None = None) -> MatrixStorage:
         a MatrixStorage object
     """
     if matrix is not None:
-        matrixStack = MatrixStorage(matrix)
-        return matrixStack
+        matrix_stack = MatrixStorage(matrix)
+        return matrix_stack
     else:
         return MatrixStorage()
 
-def main():
-    if len(sys.argv) != 3:
-        print("python main.py <rows> <count>")
-        exit(1)
-    else: 
-        print("testing")
+# def do_operation(matrix : Matrix, matrix_stack : MatrixStorage, operation_value : int) -> None:
+#     """
+#     Does the specified operation found in main()
 
+#     `params:` 
+#         matrix {Matrix} : a Matrix object
+#         operation_value {int} : a int
+#         matrix_stack {MatrixStorage} : a MatrixStorage object
+
+#     `raises:`
+#         ValueError if a operation_value isn't found
+#     """
+#     match operation_value:
+#         case 1:
+
+def main():
+    """
+    gets initial matrix, contains the loop that runs the program
+    """
+    if len(sys.argv) != 3 and len(sys.argv) != 4:
+        print("python main.py <rows> <count> <filename | Optional>")
+        exit(1)
+    elif len(sys.argv) == 3:
+        matrix : Matrix = create_matrix(int(sys.argv[1]), int(sys.argv[2]))
+    else:
+        matrix : Matrix = create_matrix(int(sys.argv[1]), int(sys.argv[2]), sys.argv(3))
+    matrix_list : List[Matrix] = [].append(matrix)
+    selected_matrix : Matrix = matrix_list[0]
+    matrix_stack : MatrixStorage = create_storage(None)
+    while True:
+        input_val : int = int(input(
+        """
+        0) Add Matrix
+        1) Select Matrix
+        2) Undo Operation
+        3) Multiply Row            
+        4) Multiply Column
+        5) Add Row to another
+        6) Add column to another
+        7) Subtract row from another
+        8) Subtract col from another
+        9) Transpose Matrix
+        10) Find Determinant  
+        """))
+        match input_val:
+            case 0:
+                input_matrix_dim : str = input("Please enter <row> <col> <filename | Optional>")
+                if len(input_matrix_dim == 1):
+                    new_matrix : Matrix = create_matrix(input_matrix_dim)
+                elif len(input_matrix_dim) == 2:
+                    new_matrix : Matrix = create_matrix(int(input_matrix_dim[0]), int(input_matrix_dim[1]))
+                elif len(input_matrix_dim) == 3:
+                    new_matrix : Matrix = create_matrix(int(input_matrix_dim[0]), int(input_matrix_dim[1]), input_matrix_dim[2])
+                matrix_list.append(new_matrix)
+        # try:
+        #     do_operation(matrix, matrix_stack, input_val)
+        # except ValueError:
+        #     print(f"That operation ({input_val}) isn't supported!")
 
 if __name__ == "__main__":
     main()
