a0c902fd87e9cdc1b57c6645c4136605341a211c
Ashton Dong <dong.ashton@gmail.com>
2024-06-12 18:36:02 -0500

    ENHANCEMENT: rows and cols are automatically determined
    If a filename is provided, then the rows and columns count are automatically determined. Otherwise, a SyntaxError will be raised.
    
    Bugfixes:
    - Added a `= None` in the create_storage()

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from src.array import Matrix, MatrixStorage
 import sys
+from typing import List
 
-def create_matrix(rows: int, cols: int, filename: str | None) -> Matrix:
+def create_matrix(rows: int | None = None, cols: int | None = None, filename: str | None = None) -> Matrix:
     """
     Creates a matrix. If a filename is specified, reads from that file.
     If not, user input will be read in and converted. For example:
@@ -12,25 +13,33 @@ def create_matrix(rows: int, cols: int, filename: str | None) -> Matrix:
     ```
 
     `params:`
-        rows {int}: the amount of rows
-        cols {int}: the amount of columns
+        rows {int}: the amount of rows, optional if filename is given
+        cols {int}: the amount of columns, optional if filename is given
         filename {str}: the filename to read from, optional
 
     `returns:` 
         a Matrix object
     
     `raises:` 
+        SyntaxError if a a filename is not provided and rows and cols also aren't provided
         ValueError if a input value can't be converted into a float
     """
     if filename is not None:
-        file = open(filename)
-        float_list = [float(num) for num in file.read().split()]
-        file.close()
+        with open(filename, 'r', encoding='UTF-8') as file:
+            lines = [line.strip().split() for line in file.readlines()]
+            if not rows:
+                rows = len(lines)
+            if not cols:
+                cols = len(lines[0])
+            float_list = [float(num) for line in lines for num in line.split()]
     else:
-        float_list = list(map(float, input("Matrix Values:\n")))
+        if rows is None or cols is None:
+            raise SyntaxError("Rows and columns must be specified for manual input.")
+        matrix_values : List[str] = input("Enter the matrix values separated by spaces:\n").split()
+        float_list = [float(num) for num in matrix_values]
     return Matrix(rows, cols, float_list)
 
-def create_storage(matrix: Matrix | None) -> MatrixStorage:
+def create_storage(matrix: Matrix | None = None) -> MatrixStorage:
     """
     Creates a matrix stack
 
